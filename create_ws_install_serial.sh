2ea49c432a69734a1b5534840360d5fb9d839ae8
Mik6e6 <mikesh@outlook.com>
2020-07-04 11:50:18 -0400

    Create create_ws_install_serial.sh

---
+++ create_ws_install_serial.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# this script sources and create a new catkin_ws and then install dependicies for rosserial 
+
+
+if [[ "${UID}" -ne 0 ]]
+then 
+  echo 'Please run as sudo'
+  exit 1 
+ fi 
+ 
+ source /opt/ros/kinetic/setup.bash
+ 
+ mkdir -p ~/catkin_ws/src 
+ 
+ cd ~/catkin_ws/
+ 
+ 
+ catkin_make
+ 
+ 
+ 
+ source devel/setup.bash 
+ 
+ 
+ echo $ROS_PACKAGE_PATH
+ 
